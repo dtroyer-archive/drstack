e2cbeffea1407504bdd154c3bbe701458ae79f2d
Dean Troyer <dtroyer@gmail.com>
2012-02-09 15:16:52 -0600

    Add some actual shell tests

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -9,16 +9,29 @@ from novaclient import exceptions
 from tests import utils
 
 
+DEFAULT_AUTH_URL = 'http://127.0.0.1:5000/v2.0/'
+DEFAULT_TENANT_NAME = 'tenant_name'
+DEFAULT_USERNAME = 'username'
+DEFAULT_PASSWORD = 'password'
+
+AUTH_ARGS = {
+    'auth_url': DEFAULT_AUTH_URL,
+    'auth_token': '',
+    'tenant_name': DEFAULT_TENANT_NAME,
+    'username': DEFAULT_USERNAME,
+    'password': DEFAULT_PASSWORD}
+
+
 class ShellTest(utils.TestCase):
 
     # Patch os.environ to avoid required auth info.
     def setUp(self):
         global _old_env
         fake_env = {
-            'OS_AUTH_URL': 'http://127.0.0.1:5000/v2.0',
-            'OS_TENANT_NAME': 'tenant_name',
-            'OS_USERNAME': 'username',
-            'OS_PASSWORD': 'password',
+            'OS_AUTH_URL': DEFAULT_AUTH_URL,
+            'OS_TENANT_NAME': DEFAULT_TENANT_NAME,
+            'OS_USERNAME': DEFAULT_USERNAME,
+            'OS_PASSWORD': DEFAULT_PASSWORD,
         }
         _old_env, os.environ = os.environ, fake_env.copy()
 
@@ -28,9 +41,17 @@ class ShellTest(utils.TestCase):
         _shell = drstack_shell
         shell = lambda cmd: _shell.main([__file__] + cmd.split())
 
+        # Patch out some common methods
+        self.kc_patch = mock.patch('drstack.shell.DrStack._get_keystone')
+        self.kc_save = self.kc_patch.start()
+        self.nc_patch = mock.patch('drstack.shell.DrStack._get_nova')
+        self.nc_save = self.nc_patch.start()
+
     def tearDown(self):
         global _old_env
         os.environ = _old_env
+        self.kc_patch.stop()
+        self.nc_patch.stop()
 
     def test_help_unknown_command(self):
         self.assertRaises(exceptions.CommandError, shell, 'foo foo')
@@ -39,3 +60,30 @@ class ShellTest(utils.TestCase):
         httplib2.debuglevel = 0
         shell('--debug help')
         assert httplib2.debuglevel == 1
+
+    def test_shell_args(self):
+
+        def test_args(desc, default_args):
+            onecmd_mock = mock.Mock(desc)
+            with mock.patch('drstack.shell.DrStack.onecmd', onecmd_mock):
+                shell('list user')
+                onecmd_mock.assert_called_with(('list user'))
+                auth_mock = mock.Mock(desc)
+                with mock.patch('drstack.shell.DrStack.set_auth', auth_mock):
+                    shell('list user')
+                    assert auth_mock.call_args == ((), default_args)
+                    shell('--auth_url http://0.0.0.0:5000/ --tenant_name fred '
+                          '--username barney --password xyzpdq '
+                          'list user')
+                    assert auth_mock.call_args == ((), {
+                        'auth_url': 'http://0.0.0.0:5000/',
+                        'auth_token': '',
+                        'tenant_name': 'fred',
+                        'username': 'barney',
+                        'password': 'xyzpdq'})
+
+        test_args('default environment', AUTH_ARGS)
+        save_env, os.environ = os.environ, {}
+        test_args('no environment', {'auth_url': '', 'auth_token': '',
+                'tenant_name': '', 'username': '', 'password': ''})
+        os.environ = save_env
